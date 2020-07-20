4b35a2c1bd38266fe79b3c1d70270059984dd0cd
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-20 19:50:44 +0800

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import Search from "./pages/search";
+import Login from "./pages/login";
+import Create from "./pages/create";
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Container>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path={["/", "/login"]}>
+              <Login />
+            </Route>
+            <Route exact path={"/search"}>
+              <Search />
+            </Route>
+            <Route exact path={"/create"}>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Container>
   );
 }
 
